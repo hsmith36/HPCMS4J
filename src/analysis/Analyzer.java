4b83a1899709634cfe89cd2109232a9e914fc04b
Hayden Smith <haydensmith36@gmail.com>
2015-04-30 11:38:53 -0600

    changing name of analyzer

--- src/analysis/Analyzer.java
+++ src/analysis/Analyzer.java
@@ -1,6 +1,6 @@
-package cms;
+package analysis;
 
-public class CMS {
+public class Analyzer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ src/analysis/Combiner.java
@@ -0,0 +1,16 @@
+package analysis;
+
+public class Combiner {
+
+	/**
+	 * Gets all the data from all the stats output files
+	 * If directly called from main it will print the data (any combination of tests) into one big file
+	 * If called from Analyzer it will return List<WindowStats> for further analysis
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
